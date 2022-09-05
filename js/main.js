b3c3f52426cc88edf5f8da8206cd294cd46fa7c1
Daniru2007 <daniruayuka@gmail.com>
2022-09-05 21:25:26 +0530

    making the site look beautiful!

--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ function help() {
     addLine("clear - clear the hell");
     addLine("whoami - details about the devil");
     addLine("bloodfetch - wanna see some blood?");
+    addLine("where - meet the devil");
+    addLine("ls - list of projects");
+    addLine("contact - calling devil is illegal!");
 }
 
 function clear() {
@@ -45,11 +48,76 @@ function bloodfetch() {
     output.appendChild(center);
 }
 
+function where() {
+    window.open("https://github.com/Daniru2007");
+    addLine("sending to devil...");
+}
+
+function addLink(text, desc, link) {
+    a = document.createElement("a");
+    a.append(`(${text}) [${desc}]`);
+    a.setAttribute("href", link);
+    a.setAttribute("target", "_blank");
+    output.appendChild(a);
+    output.appendChild(document.createElement("br"));
+}
+
+function ls() {
+    addLink(
+        "portfolio",
+        "My Portfolio",
+        "https://github.com/Daniru2007/portfolio"
+    );
+    addLink(
+        "darknights chess website",
+        "a chess website for darknights chess academy",
+        "https://github.com/Daniru2007/darknights-chess"
+    );
+    addLink(
+        "todolist app",
+        "task management app",
+        "https://github.com/Daniru2007/todo-list"
+    );
+    addLink(
+        "typing app",
+        "typing practice app",
+        "https://github.com/Daniru2007/typing-app"
+    );
+    addLink(
+        "fb clone",
+        "facebook clone",
+        "https://github.com/Daniru2007/fb-clone"
+    );
+    addLink("tada app", "todo list app", "https://github.com/Daniru2007/tada");
+    addLink(
+        "github repo cli",
+        "automated github project creator",
+        "https://github.com/Daniru2007/github-repo-cli"
+    );
+    addLink(
+        "dotfiles",
+        "My DotFiles",
+        "https://github.com/Daniru2007/DotFiles"
+    );
+}
+
+function contact() {
+    a = document.createElement("a");
+    a.append(`github`);
+    a.setAttribute("href", "https://github.com/Daniru2007");
+    a.setAttribute("target", "_blank");
+    output.appendChild(a);
+    output.appendChild(document.createElement("br"));
+}
+
 const commands = {
     whoami: whoami,
     clear: clear,
     help: help,
     bloodfetch: bloodfetch,
+    where: where,
+    ls: ls,
+    contact: contact,
 };
 
 function commandEnter(e) {
@@ -62,7 +130,7 @@ function commandEnter(e) {
             addLine("devil@hell~$");
         } else {
             addLine(`devil@hell~$ ${commander.value}`);
-            addLine(`Error`);
+            addLine(`Command not found. For a list of commands, type 'help'.`);
         }
         window.scrollTo(0, document.body.scrollHeight);
 
